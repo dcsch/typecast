af0594184d67e511e2252b11343f19fac92281f9
David Schweinsberg <david.schweinsberg@gmail.com>
2004-12-21 16:56:54 +0000

    Now implemented

--- src/net/java/dev/typecast/ot/table/CmapFormat2.java
+++ src/net/java/dev/typecast/ot/table/CmapFormat2.java
@@ -54,35 +54,120 @@ import java.io.DataInput;
 import java.io.IOException;
 
 /**
- * TODO: To be implemented
- * @version $Id: CmapFormat2.java,v 1.2 2004-12-21 10:22:55 davidsch Exp $
+ * High-byte mapping through table cmap format.
+ * @version $Id: CmapFormat2.java,v 1.3 2004-12-21 16:56:54 davidsch Exp $
  * @author <a href="mailto:davidsch@dev.java.net">David Schweinsberg</a>
  */
 public class CmapFormat2 extends CmapFormat {
 
-    private short[] _subHeaderKeys = new short[256];
-    private int[] _subHeaders1;
-    private int[] _subHeaders2;
-    private short[] _glyphIndexArray;
+    private class SubHeader {
+        int _firstCode;
+        int _entryCount;
+        short _idDelta;
+        int _idRangeOffset;
+        int _arrayIndex;
+    }
+    
+    private int[] _subHeaderKeys = new int[256];
+    private SubHeader[] _subHeaders;
+    private int[] _glyphIndexArray;
 
     protected CmapFormat2(DataInput di) throws IOException {
         super(di);
         _format = 2;
         
-        // HACK: As this is not yet implemented, we need to skip over the bytes
-        // we should be consuming
-        di.skipBytes(_length - 4);
+        int pos = 6;
+        
+        // Read the subheader keys, noting the highest value, as this will
+        // determine the number of subheaders to read.
+        int highest = 0;
+        for (int i = 0; i < 256; ++i) {
+            _subHeaderKeys[i] = di.readUnsignedShort();
+            highest = Math.max(highest, _subHeaderKeys[i]);
+            pos += 2;
+        }
+        int subHeaderCount = highest / 8 + 1;
+        _subHeaders = new SubHeader[subHeaderCount];
+        
+        // Read the subheaders, once again noting the highest glyphIndexArray
+        // index range.
+        int indexArrayOffset = 8 * subHeaderCount + 518;
+        highest = 0;
+        for (int i = 0; i < _subHeaders.length; ++i) {
+            SubHeader sh = new SubHeader();
+            sh._firstCode = di.readUnsignedShort();
+            sh._entryCount = di.readUnsignedShort();
+            sh._idDelta = di.readShort();
+            sh._idRangeOffset = di.readUnsignedShort();
+            
+            // Calculate the offset into the _glyphIndexArray
+            pos += 8;
+            sh._arrayIndex =
+                    (pos - 2 + sh._idRangeOffset - indexArrayOffset) / 2;
+            
+            // What is the highest range within the glyphIndexArray?
+            highest = Math.max(highest, sh._arrayIndex + sh._entryCount);
+            
+            _subHeaders[i] = sh;
+        }
+        
+        // Read the glyphIndexArray
+        _glyphIndexArray = new int[highest];
+        for (int i = 0; i < _glyphIndexArray.length; ++i) {
+            _glyphIndexArray[i] = di.readUnsignedShort();
+        }
     }
 
     public int getRangeCount() {
-        return 0;
+        return _subHeaders.length;
     }
     
     public Range getRange(int index) throws ArrayIndexOutOfBoundsException {
-        throw new ArrayIndexOutOfBoundsException();
+        if (index < 0 || index >= _subHeaders.length) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        
+        // Find the high-byte (if any)
+        int highByte = 0;
+        if (index != 0) {
+            for (int i = 0; i < 256; ++i) {
+                if (_subHeaderKeys[i] / 8 == index) {
+                    highByte = i << 8;
+                    break;
+                }
+            }
+        }
+        
+        return new Range(
+                highByte | _subHeaders[index]._firstCode,
+                highByte | (_subHeaders[index]._firstCode +
+                    _subHeaders[index]._entryCount - 1));
     }
 
     public int mapCharCode(int charCode) {
-        return 0;
+        
+        // Get the appropriate subheader
+        int index = 0;
+        int highByte = charCode >> 8;
+        if (highByte != 0) {
+            index = _subHeaderKeys[highByte] / 8;
+        }
+        SubHeader sh = _subHeaders[index];
+        
+        // Is the charCode out-of-range?
+        int lowByte = charCode & 0xff;
+        if (lowByte < sh._firstCode ||
+                lowByte >= (sh._firstCode + sh._entryCount)) {
+            return 0;
+        }
+        
+        // Now calculate the glyph index
+        int glyphIndex =
+                _glyphIndexArray[sh._arrayIndex + (lowByte - sh._firstCode)];
+        if (glyphIndex != 0) {
+            glyphIndex += sh._idDelta;
+            glyphIndex %= 65536;
+        }
+        return glyphIndex;
     }
 }
